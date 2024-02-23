6f5a6c926597c735f7786b667a04e8d443681fc5
root <sina@hyperdbg.org>
2024-02-23 15:28:18 +0900

    add sample top module

---
+++ src/main/scala/top.scala
@@ -0,0 +1,28 @@
+package hwdbg
+
+import chisel3._
+import chisel3.util.Counter
+import circt.stage.ChiselStage
+
+class Blinky(freq: Int, startOn: Boolean = false) extends Module {
+  val io = IO(new Bundle {
+    val led0 = Output(Bool())
+  })
+  // Blink LED every second using Chisel built-in util.Counter
+  val led = RegInit(startOn.B)
+  val (_, counterWrap) = Counter(true.B, freq / 2)
+  when(counterWrap) {
+    led := ~led
+  }
+  io.led0 := led
+}
+
+object Main extends App {
+  // These lines generate the Verilog output
+  println(
+    ChiselStage.emitSystemVerilog(
+      new Blinky(1000),
+      firtoolOpts = Array("-disable-all-randomization", "-strip-debug-info")
+    )
+  )
+}
